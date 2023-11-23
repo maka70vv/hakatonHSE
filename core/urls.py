3db62b5610d7fdea4a1e65683399d5edbe532c84
makarov <25.makarovv@gmail.com>
2023-11-23 16:47:26 +0600

    urls fixing

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("current-user-devices/", DevicesByUserView.as_view()),
     path('register/', RegisterView.as_view()),
     path('user-devices/<int:pk>/', DevicesDetailView.as_view()),
-    path('windows_open/<int:device_id>/', toggle_windows_status),
-    path('windows_block/<int:device_id>/', block_windows),
+    path('windows-open/<int:device_id>/', toggle_windows_status),
+    path('windows-block/<int:device_id>/', block_windows),
 ]
