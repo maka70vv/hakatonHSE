3c211e44627f6706084f788d7a8d6b752b929061
makarov <25.makarovv@gmail.com>
2023-11-23 16:49:38 +0600

    urls fixing

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import DevicesView, WindowsView, RegisterView, DevicesByUserView, DevicesDetailView, toggle_windows_status, \
-    block_windows
+from .views import DevicesView, WindowsView, RegisterView, DevicesByUserView, DevicesDetailView, \
+    toggle_windows_status, block_windows
 
 urlpatterns = [
     path("arduino-devices/", DevicesView.as_view()),
