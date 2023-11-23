ebe677057d09e5195c3084784ee6143d854b6bd4
makarov <25.makarovv@gmail.com>
2023-11-23 16:46:42 +0600

    close and open/block windows by id

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import DevicesView, WindowsView, RegisterView, DevicesByUserView
+from .views import DevicesView, WindowsView, RegisterView, DevicesByUserView, DevicesDetailView, toggle_windows_status, \
+    block_windows
 
 urlpatterns = [
     path("arduino-devices/", DevicesView.as_view()),
     path("user-devices/", WindowsView.as_view()),
     path("current-user-devices/", DevicesByUserView.as_view()),
     path('register/', RegisterView.as_view()),
-
+    path('user-devices/<int:pk>/', DevicesDetailView.as_view()),
+    path('windows_open/<int:device_id>/', toggle_windows_status),
+    path('windows_block/<int:device_id>/', block_windows),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, permission_classes
+
 from .models import ArduinoDevices, Devices
 from .serializers import ArduinoDevicesSerializers, DevicesSerializers, RegisterSerializer, \
     UserSerializer, WindowsListSerializer
@@ -10,7 +12,7 @@ class RegisterView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = RegisterSerializer
 
-    def post(self, request, *args,  **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
@@ -36,3 +38,59 @@ class WindowsView(generics.ListCreateAPIView):
     queryset = Devices.objects.all()
     serializer_class = DevicesSerializers
     permission_classes = [permissions.IsAuthenticated]
+
+
+class DevicesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Devices.objects.all()
+    serializer_class = DevicesSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != request.user:
+            return Response({"error": "Нет прав для удаления этого устройства"}, status=status.HTTP_403_FORBIDDEN)
+        return self.destroy(request, *args, **kwargs)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def toggle_windows_status(request, device_id):
+    try:
+        device = Devices.objects.get(pk=device_id, user=request.user)
+    except Devices.DoesNotExist:
+        return Response({"error": "Устройство не найдено"}, status=status.HTTP_404_NOT_FOUND)
+
+    if not device.windowsAreBlocked and not device.windowsAreOpened:
+        device.windowsAreOpened = True
+        device.save()
+        serializer = DevicesSerializers(device)
+        return Response(serializer.data)
+    elif device.windowsAreOpened:
+        device.windowsAreOpened = False
+        device.save()
+        serializer = DevicesSerializers(device)
+        return Response(serializer.data)
+    elif not device.windowsAreOpened and device.windowsAreBlocked:
+        return Response({"error": "Окна заблокированы, невозможно открыть"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def block_windows(request, device_id):
+    try:
+        device = Devices.objects.get(pk=device_id, user=request.user)
+    except Devices.DoesNotExist:
+        return Response({"error": "Устройство не найдено"}, status=status.HTTP_404_NOT_FOUND)
+
+    if not device.windowsAreBlocked and not device.windowsAreOpened:
+        device.windowsAreBlocked = True
+        device.save()
+        serializer = DevicesSerializers(device)
+        return Response(serializer.data)
+    elif not device.windowsAreOpened and device.windowsAreBlocked:
+        device.windowsAreBlocked = False
+        device.save()
+        serializer = DevicesSerializers(device)
+        return Response(serializer.data)
+    elif device.windowsAreOpened and not device.windowsAreBlocked:
+        return Response({"error": "Окна открыты! Закройте, чтобы заблокировать"}, status=status.HTTP_400_BAD_REQUEST)
